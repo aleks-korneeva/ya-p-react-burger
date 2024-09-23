1d34ea3bf4d545c38597b5c7909aaec7e517491b
Aleksandra Korneeva <roxette9188@gmail.com>
2024-09-23 22:22:19 +0300

    dev: center content

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -7,19 +7,18 @@ const AppHeader = () => {
         <header>
             <div className={styles.nav}>
                 <nav className={styles.menu_container}>
-                    <a href="" className={styles.menu_item}>
+                    <a href="/" className={styles.menu_item}>
                         <BurgerIcon type="primary"/>
                         <p className={"text text_color_primary ml-2"}>Конструктор</p>
                     </a>
 
-                    <a href="" className={styles.menu_item}>
+                    <a href="/" className={styles.menu_item}>
                         <ListIcon type="secondary"/>
                         <p className={"text text_color_inactive ml-2"}>Лента заказов</p>
                     </a>
                 </nav>
-
                 <Logo/>
-                <a href="" className={styles.menu_item}>
+                <a href="/" className={styles.menu_item}>
                     <ProfileIcon type="secondary"/>
                     <p className={"text text_color_inactive ml-2"}>Личный кабинет</p>
                 </a>
